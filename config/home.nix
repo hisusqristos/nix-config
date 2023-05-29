523dc73f5b53a90a67bbaca7921cf11c97257e03
blasteroid <varyadaser@gmail.com>
2023-05-29 15:24:07 +0400

    :package: web server to serve static files

--- config/home.nix
+++ config/home.nix
@@ -8,6 +8,23 @@
 
   home-manager.users.blasteroid = { pkgs, ... }: {
     home.stateVersion = "22.11";
-    home.packages = [ pkgs.node2nix ];
+    home.packages = with pkgs;
+      [
+        node2nix
+        htop
+
+        # npm packages
+        nodePackages."http-server"
+      ];
+
+    # programs.rofi = {
+    #   enable = true;
+    #   theme = theme;
+    # };
+
+
+    programs.home-manager = {
+      enable = true;
+    };
   };
 }
